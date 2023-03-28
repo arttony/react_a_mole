84cf63bbd85b9ff7390e1f2867759b240cf9a698
Art Ramirez <artramirez08@gmail.com>
2023-03-27 19:44:01 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import MoleContainer from "./MoleContainer";
+
+function App() {
+  let [score, setScore] = useState(0);
+  const createMoleHill = () => {
+    let hills = [];
+    for (let i = 0; i < 9; i++) {
+      hills.push(<MoleContainer key={i} score={score} setScore={setScore} />);
+    }
+    return <div>{hills}</div>;
+  };
+  return (
+    <div className="App">
+      <p>React-a-Mole!</p>
+      {score}
+      {createMoleHill()}
+    </div>
+  );
+}
+// Still need handleClick!
+export default App;
